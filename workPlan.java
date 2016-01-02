3f75f55645e8b858a1c5bb8594a9992f32b2a2b1
itai-m <Itaimichael@gmail.com>
2016-01-02 22:50:04 +0200

    add some function for the workPlan class

--- workPlan.java
+++ workPlan.java
@@ -8,25 +8,67 @@ import com.custommods.walkmod.Step;
 
 import net.minecraft.item.ItemStack;
 
-public class workPlan {
+public class WorkPlan {
 
 	public static enum type {smelt, craft};
 	private List list;
 	
 	///Constructor
-	public workPlan(){
+	public WorkPlan(){
 		this.list = new ArrayList();
 	}
 	
+	///Add item to Craft
 	public void add (ItemStack item){
-		
+		list.add(item);
 	}
 	
+	///Add Queue of steps
 	public void add (Queue<Step> steps){
-		
+		list.add(steps);
 	}
 	
-	public void add (){
-		
+	///Check if the plan is empty
+	public boolean isEmpty(){
+		return list.isEmpty();
 	}
+	
+	///Remove the last one in the plan
+	public void removeLast(){
+		list.remove(list.lastIndexOf(list));
+	}
+	
+	///Peek the last one in the plan
+	public Object peekFirst(){
+		return list.get(0);
+	}
+	
+	///Pull the last one in the plan
+	public Object pullFirst(){
+		Object toReturn  = list.get(0);
+		list.remove(0);
+		return toReturn;
+	}
+	
+	///Return the string with all the plan
+	public String toString(){
+		String toReturn = "";
+		int index = 0;
+		if (list.isEmpty()){
+			return "Plan is empty";
+		}
+		for (Object object : list) {
+			if (object instanceof ItemStack){
+				toReturn += index++ + ": Craft- " + ((ItemStack) object).getDisplayName() + "\n";
+			}
+			else if (object instanceof Queue){
+				toReturn += index++ + ": Move to- " + ((Step) ((Queue)object).peek()).getLocation() + "\n";
+			}
+			else{
+				toReturn += "Not Found \n";
+			}
+		}
+		return toReturn;
+	}
+
 }
