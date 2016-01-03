0b3f1b3e64b074be08a3b0cc9825b67c784e49e0
itai-m <Itaimichael@gmail.com>
2016-01-03 20:58:20 +0200

    add isContain function

--- Util.java
+++ Util.java
@@ -63,4 +63,18 @@ public class Util{
 		return heur;
 	}
 	
+	///Check if one item containing the other
+	public static boolean isContain(ItemStack item1, ItemStack item2){
+		if (Item.getIdFromItem(item1.getItem()) == Item.getIdFromItem(item2.getItem())){
+			if (item1.stackSize >= item2.stackSize){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	}
 }
