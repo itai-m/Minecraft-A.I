188de6978aee0c3b52b5b4866f7e5e3b29d13723
itai-m <Itaimichael@gmail.com>
2016-01-13 15:22:35 +0200

    add used item to tge workPlan class

--- workPlan.java
+++ workPlan.java
@@ -6,16 +6,20 @@ import java.util.Queue;
 
 import com.custommods.walkmod.Step;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class WorkPlan {
 
 	public static enum type {smelt, craft};
+	public static int NOT_FOUND = -1;
 	private List list;
+	private List usedItems;
 	
 	///Constructor
 	public WorkPlan(){
 		this.list = new ArrayList();
+		this.usedItems = new ArrayList<ItemStack>();
 	}
 	
 	///Add item to Craft
@@ -31,6 +35,61 @@ public class WorkPlan {
 	///Check if the plan is empty
 	public boolean isEmpty(){
 		return list.isEmpty();
+	}
+	
+	///Add item that is used
+	public void addUsedItem(ItemStack item){
+		usedItems.add(item);
+	}
+	
+	///Remove an item from the used
+	public boolean removeUsedItem(ItemStack item){
+		int stack = item.stackSize;
+		for (Object object : usedItems) {
+			if (object instanceof ItemStack){
+				if (Item.getIdFromItem(item.getItem()) == Item.getIdFromItem(((ItemStack) object).getItem())){
+					if (((ItemStack) object).stackSize <= stack){
+						stack -= ((ItemStack) object).stackSize;
+						usedItems.remove(object);
+						if (stack==0){
+							return true;
+						}
+					}
+					else{
+						((ItemStack) object).stackSize -= stack;
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	///Get the number of stack of the item been used, return NOT_FOUND if the item not found
+	public int getStackInUsed(ItemStack item){
+		int conter = 0;
+		for (Object object : usedItems) {
+			if (object instanceof ItemStack){
+				if (Item.getIdFromItem(item.getItem()) == Item.getIdFromItem(((ItemStack) object).getItem())){
+					conter += ((ItemStack) object).stackSize;
+				}
+			}
+		}
+		if (conter == 0){
+			return conter;
+		}
+		return NOT_FOUND;
+	}
+	
+	///Check if can used an item
+	public boolean canUsedItem(ItemStack item, AIinventory inve){
+		int stack = 0;
+		ItemStack newItem = new ItemStack(item.getItem());
+		newItem.stackSize = item.stackSize;
+		if ((stack = getStackInUsed(item))!=NOT_FOUND){
+			newItem.stackSize += stack;
+		}
+		return inve.haveItem(newItem);
 	}
 	
 	///Remove the last one in the plan
